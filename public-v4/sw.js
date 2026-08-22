cd8201e0a33e1b0e348361443eaff107bc7f26b3
tavernagamesinfo-cpu <tavernagamesinfo@gmail.com>
2026-08-22 18:44:27 +0200

    Add v4 service worker

---
+++ public-v4/sw.js
@@ -0,0 +1,5 @@
+const CACHE='autoverifica-v4';
+const CORE=['/','/manifest.webmanifest','/icon-192.png','/icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}))});
+self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||new URL(e.request.url).pathname.startsWith('/api/'))return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/'))))});
